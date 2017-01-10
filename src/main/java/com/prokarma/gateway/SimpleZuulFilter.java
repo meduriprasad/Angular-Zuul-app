8380215e2e3e55288b593de393fef9d99622eff8
ProKarma Technology Office <bkumar@prokarma.com>
2017-01-10 15:39:34 +0530

    Added angular app for async event handler with zuul

---
+++ src/main/java/com/prokarma/gateway/SimpleZuulFilter.java
@@ -0,0 +1,36 @@
+package com.prokarma.gateway;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Component;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+@Component
+public class SimpleZuulFilter extends ZuulFilter {
+
+	@Override
+	public Object run() {
+		 RequestContext ctx = RequestContext.getCurrentContext();
+		    ctx.addZuulRequestHeader("Content-type", "application/json");
+
+		
+		    return null;
+	}
+
+	@Override
+	public boolean shouldFilter() {
+		return true;
+	}
+
+	@Override
+	public int filterOrder() {
+		return 1;
+	}
+
+	@Override
+	public String filterType() {
+		return "pre";
+	}
+
+}
